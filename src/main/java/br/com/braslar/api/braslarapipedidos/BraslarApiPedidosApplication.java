b62175f7b3ab300f9b72bc822d5e265e8397f16e
Igor <igoreduardo20101@gmail.com>
2024-05-20 14:22:12 -0300

    Initial commit

---
+++ src/main/java/br/com/braslar/api/braslarapipedidos/BraslarApiPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.braslar.api.braslarapipedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraslarApiPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraslarApiPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/braslar/api/braslarapipedidos/BraslarApiPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.braslar.api.braslarapipedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraslarApiPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
